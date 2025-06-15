420b76634abd0ed8be852e77d3700b6350d23ab9
AKENTEVDA <165480676+AKENTEVDA@users.noreply.github.com>
2025-06-15 22:32:17 +0400

    projects-page yuga-section

--- projects/projects.js
+++ projects/projects.js
@@ -78,3 +78,77 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 300);
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const wrapper = document.querySelector(".twinPeaks__wrapper");
+  const images = document.querySelectorAll(".twinPeaks__img");
+  const leftArrow = document.querySelector(".twinPeaks__icon-left");
+  const rightArrow = document.querySelector(".twinPeaks__icon-right");
+  let currentIndex = 0;
+
+  updateSlider();
+
+  function updateSlider() {
+    images.forEach((img, index) => {
+      img.style.opacity = index === currentIndex ? "1" : "0";
+      img.style.zIndex = index === currentIndex ? "2" : "1";
+      img.style.transform = "translateX(0)";
+    });
+  }
+
+  function slide(direction) {
+    images[currentIndex].style.opacity = "0";
+    images[currentIndex].style.zIndex = "1";
+
+    if (direction === "right") {
+      currentIndex = (currentIndex + 1) % images.length;
+      images[currentIndex].style.transform = "translateX(100%)";
+    } else {
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      images[currentIndex].style.transform = "translateX(-100%)";
+    }
+
+    images[currentIndex].style.opacity = "1";
+    images[currentIndex].style.zIndex = "2";
+
+    setTimeout(() => {
+      images[currentIndex].style.transform = "translateX(0)";
+    }, 10);
+  }
+
+  rightArrow.addEventListener("click", () => {
+    slide("right");
+  });
+
+  leftArrow.addEventListener("click", () => {
+    slide("left");
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowRight") slide("right");
+    if (e.key === "ArrowLeft") slide("left");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const plusIcon = document.querySelector(".kidIt-icon");
+  const textBlock = document.querySelector(".kidIt__text");
+
+  plusIcon.addEventListener("click", function () {
+    textBlock.classList.toggle("visible");
+    plusIcon.classList.toggle("rotated");
+
+    event.stopPropagation();
+  });
+
+  document.addEventListener("click", function () {
+    if (textBlock.classList.contains("visible")) {
+      textBlock.classList.remove("visible");
+      plusIcon.classList.remove("rotated");
+    }
+  });
+
+  textBlock.addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
+});
